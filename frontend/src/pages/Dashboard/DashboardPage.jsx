bfa5ced526b05a56f13517f5831a4c53da4ea9f4
Shahriyar-Rahim <rahimislam420.ri35@gmail.com>
2026-06-08 01:00:35 +0600

    added CT Folder

--- frontend/src/pages/Dashboard/DashboardPage.jsx
+++ frontend/src/pages/Dashboard/DashboardPage.jsx
@@ -15,7 +15,7 @@ const FOLDER_TYPES = [
     color: "bg-red-50    border-red-200    text-red-700",
   },
   {
-    category: "Mid",
+    category: "Mid + CT",
     icon: "📝",
     color: "bg-blue-50   border-blue-200   text-blue-700",
   },
